a4d8070a10e67f9e379fddba7db8a1a944a35d34
worlcrafte <leo.flandin10@orange.fr>
2024-02-06 12:08:52 +0100

    ajout d'une nvlle fct

--- main/grid_algo.m
+++ main/grid_algo.m
@@ -1,21 +1,24 @@
-function sigmin = grid_algo(A, m)
-    %g(x,y) = 1/norm(((x+y)))
-    %z(0) = newtons_method()
+function sigmin = grid_algo(A, m, epsilon, F,tho)
+    u = linspace(-1,1,m);
+    %x = linspace(-1.5,1.5, m);
+    y = linspace(-1.5,1.5, m);
+    Au =  @(x) sqrt(1i*F/pi) * integral(exp(-1i*F*(x-y).^2)*u(y), 0 , 1 );
+    z = zeros(m);
+    diff(Au);
+    Au = Au-A;
+    [~, U, V] = svd(Au);
+    for k = 2:m
+        z(k) = z(k - 1) + tho * i(V * U) / norm(V * U);
+        [S, U, V] = svd(z(k) - A);
+        z(k) = z(k) - (S - epsilon) / (u * v);
+    end
+    sigmin = z;
+    %[N,~] = size(A);
     
-    %[S, U, V] = svd(z(1) - A);
-
-    %for k = 2:N
-     %   z(k) = z(k - 1) + tho * i(v * u) / norm(v * u);
-      %  [S, U, V] = svd(z(k) - A);
-       % z(k) = z(k) - (S - e) / (u * v);
-        %plot(z);
-    %end
-    %sigmin = z;
-    [N,~] = size(A);
-    x = logspace(-1,1, m);
-    y = logspace(-1,1, m);
-    for k=1:m , for j = 1:m
-        sigmin(j,k) = min(svd((x(k)+y(j)*1i*eye(N)-A)));
-    end,end
+    
+    
+    %for k=1:m , for j = 1:m
+    %    sigmin(j,k) = min(svd((x(k)+y(j)*1i*eye(N)-A)));
+    %end,end
     contour(x,y,log10(sigmin));
 end
