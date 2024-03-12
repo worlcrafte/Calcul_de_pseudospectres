3d5e189360caa2104bd069699380cd347726e40b
worlcrafte <leo.flandin10@orange.fr>
2024-03-12 15:31:30 +0100

    commentaire de la fonction ajouté

--- main/stress_test.m
+++ main/stress_test.m
@@ -1,7 +1,27 @@
 function stress_test(size_from,size_to,step,m_point_to_evaluate,thread_from, thread_to,rand_function,epsilon)
-%STRESS_TEST Summary of this function goes here
-%   Detailed explanation goes here
+% input : 
+%       - size_from :   size of the matrix at the begining
+%       - size_to   :   size of the matrix at the end of the test
+%       - step      :   step for the size of the matrix 
+%       - m_point_to_evaluate   :   the number of point to evaluate for the
+%       pseudospectra algo
+%       - thread_from   :   number of thread at the begining of the test
+%       - thread_to     :   numbre of thread at the end of the test 
+%       - rand_function :   function used to generate the matrix
+%       - algo_used_for_pseudospectra : function used to compute the
+%       pseudospectra of the matrix
+
+% output    :   none 
+
+%   Explanation :
+%       Generate a plot to evaluate the performance of the algorithm
+%       depending on the size of the matrix and the number of size used. We
+%       will add the possibility to choose the algo used and to compare the
+%       algo the user can use.
+
+    %generate the matrix 
     A = rand_function(size_from);
+
     size_matrix = size_from:step:size_to;
     [n,~] = size(size_matrix);
     time_mesurment = zeros(thread_to-thread_from,n);
@@ -11,10 +31,11 @@ function stress_test(size_from,size_to,step,m_point_to_evaluate,thread_from, thr
     % delete the previous parpool to create a new one with all possible
     % workers.
     if ~ isempty(p)
-        %delete(p);
+        delete(p);
     end
-    %parpool();
-
+    parpool();
+    
+    %   time sampling
     for n = size_matrix
         for t = thread_from:thread_to
             tic
@@ -26,15 +47,22 @@ function stress_test(size_from,size_to,step,m_point_to_evaluate,thread_from, thr
         A = rand_function(n);
         i=1;
     end 
+    % generate a vector for the x axe
     threads = thread_from:thread_to;
+    
+    % number of mesurement realised
     [~,n] =size(size_matrix);
+    
+    % vector of plots for legend
     h = zeros(n,1);
+
+    % generate plots
     h(1) = plot(threads,time_mesurment(1,:));
     hold on;
     for i = 2:n
         h(i) = plot(threads,time_mesurment(i,:));
     end
-    
+
     hold off;
 end
 
