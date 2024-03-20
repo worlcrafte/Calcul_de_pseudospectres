51d7f249d470689e09d61e7271be4ba76bf7c135
worlcrafte <leo.flandin10@orange.fr>
2024-03-20 10:40:54 +0100

    efficiency and speedup added

--- main/stress_test.m
+++ main/stress_test.m
@@ -23,56 +23,77 @@ function stress_test(size_from,size_to,step,m_point_to_evaluate,thread_from, thr
     A = rand_function(size_from);
 
     size_matrix = size_from:step:size_to;
-    [n,~] = size(size_matrix);
+    [~,n] = size(size_matrix);
     time_mesurment = zeros(thread_to-thread_from,n);
     i=1;
     j=1;
     p = gcp('nocreate');
     % delete the previous parpool to create a new one with all possible
     % workers.
-    if ~ isempty(p)
-        delete(p);
+    %if ~ isempty(p)
+    %    delete(p);
+    %end
+    %parpool();
+    begining = 1;
+    if thread_from ~=1
+        threads = [1 thread_from:thread_to];
+        begining = 2;
+    else 
+        threads =  thread_from:thread_to;
     end
-    parpool();
-    
     %   time sampling
     for n = size_matrix
-        for t = thread_from:thread_to
+        for t = threads
             tic
             gridPseudospectrum_par(A,epsilon,t,m_point_to_evaluate);
-            time_mesurment(j,i) = toc;
+            time_mesurment(i,j) = toc;
             i=i+1;
         end
         j=j+1;
         A = rand_function(n);
         i=1;
-    end 
+    end
     % generate a vector for the x axe
-    threads = thread_from:thread_to;
-    
-    % number of mesurement realised
-    [~,n] =size(size_matrix);
-    
-    % vector of plots for legend
-    h = zeros(n,1);
-    txt = "size matrix : " + string(size_matrix(1));
-    % generate plots
-    time_mesurment = time_mesurment * 10^3;
-    
-    disp(time_mesurment);
-
-    h(1) = plot(gui,threads,log10(time_mesurment(1,:)),'DisplayName',txt);
-    
-    title('Measured time');
-    xlabel('Number of thread used');
-    ylabel('log10 of the measured time in ms');
-    hold(gui,'on');
-    for i = 2:n
-        txt = "size matrix : " + string(size_matrix(i));
-        h(i) = plot(gui,threads,log10(time_mesurment(i,:)),'DisplayName',txt);
+    disp(time_mesurment)
+    [~,n] = size(size_matrix);
+    [~,m] = size(threads);
+    speedup = zeros(thread_to-thread_from+1,n);
+    efficiency = zeros(thread_to-thread_from+1,n);
+    disp(speedup);
+    i=1;
+    for j = 1:n 
+        best = time_mesurment(1,j);
+        a = best./time_mesurment(begining:m,j);
+        speedup(:,j) = a(:);
+        disp(threads(begining:m)');
+        disp(a);
+        efficiency(:,j) = a./threads(begining:m)';
+        i=i+1;
     end
     
-    hold(gui,"off");
-    legend(gui);
+    % number of mesurement realised
+    %[~,n] =size(size_matrix);
+    
+    % vector of plots for legend
+    %h = zeros(n,1);
+    %txt = "size matrix : " + string(size_matrix(1));
+    % generate plots
+    %time_mesurment = time_mesurment * 10^3;
+    
+    %disp(time_mesurment);
+
+    %h(1) = plot(gui,threads,log10(time_mesurment(1,:)),'DisplayName',txt);
+    
+    %title('Measured time');
+    %xlabel('Number of thread used');
+    %ylabel('log10 of the measured time in ms');
+    %hold(gui,'on');
+    %for i = 2:n
+     %   txt = "size matrix : " + string(size_matrix(i));
+      %  h(i) = plot(gui,threads,log10(time_mesurment(i,:)),'DisplayName',txt);
+    %end
+    
+    %hold(gui,"off");
+    %legend(gui);
 end
 
