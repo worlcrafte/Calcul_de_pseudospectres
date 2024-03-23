2a67c17b68cbddc69068224931a47101a00adc99
LashaGOG <gogritchianilasha@gmail.com>
2024-03-23 14:15:33 +0100

    Add changes to generate_matrix and curveTracing

--- main/generate_matrix.m
+++ main/generate_matrix.m
@@ -1,11 +1,8 @@
 function res = generate_matrix(N)
     mu = 1;
     sigma = 5;
-    rng('Default');
-    for i = 1:N
-        for j = 1:N
-            res(i,j) = random('Normal',mu,sigma);
-        end
-    end
+    rng('default'); % set random number generator to default
+    res = normrnd(mu, sigma, N); % generate N x N matrix of random numbers
 end
+
 
