1be407a29fc4aa6c79afa8f6201c758098be5595
worldcraft <leo.flandin10@orange.fr>
2024-05-22 17:52:46 +0200

    ajout d'une wait barre dans la mesure de perf

--- main/perf_test/stress_test_curve.m
+++ main/perf_test/stress_test_curve.m
@@ -21,12 +21,11 @@ function [time_mesurment_curve,size_matrix,threads] = stress_test_curve(size_fro
 %       algo the user can use.
 
     %generate the matrix 
-    A = rand_function(size_from);
     d0=1i;
     size_matrix = size_from:step_size:size_to;
-    [~,n] = size(size_matrix);
     i=1;
     j=1;
+    
     p = gcp('nocreate');
     % delete the previous parpool to create a new one with all possible
     % workers.
@@ -41,8 +40,10 @@ function [time_mesurment_curve,size_matrix,threads] = stress_test_curve(size_fro
     end
     disp(threads);
     %   time sampling
+    f =  waitbar(0,'Compute the data');
     for n = size_matrix
         A = rand_function(n);
+        waitbar(j/length(size_matrix),f,'Compute the data');
         for t = threads
             %tic
             %[X,Y,~] = gridPseudospectrum_par(A, epsilon,t,m_point_to_evaluate);
@@ -60,105 +61,5 @@ function [time_mesurment_curve,size_matrix,threads] = stress_test_curve(size_fro
     disp(time_mesurment_curve);
     % generate a vector for the x axe
     
-end
-
-function aa()
-    [~,n] = size(size_matrix);
-    [~,m] = size(threads);
-    speedup = zeros(thread_to-thread_from+1,n);
-    efficiency = zeros(thread_to-thread_from+1,n);
-    i=1;
-    for j = 1:n 
-        best = time_mesurment(1,j);
-        a = best./time_mesurment(begining:m,j);
-        speedup(:,j) = a(:);
-        disp(speedup);
-        efficiency(:,j) = a./threads(begining:m)';
-        i=i+1;
-    end
-
-    %Histogramme
-    figure(1);
-    disp("speedup : ");
-    disp(speedup);
-    barh(size_matrix(:).^2,speedup');
-    xlabel('speedup');
-    ylabel('size matrices');
-    t = num2cell(string(threads(begining:m)));
-    legend(t{:});
-    title_name = sprintf('Bar graph of computational speedup vs number of threads for various instance sizes with %i points to evalueate', m_point_to_evaluate);
-    title(title_name);
-
-    figure(2);
-    barh(size_matrix(:).^2,efficiency');
-    xlabel('Efficiency');
-    ylabel('size matrices');
-    %t = num2cell(string(threads(begining:m)));
-    legend(t{:});
-    title_name = sprintf('Bar graph of computational speedup vs number of threads for various instance sizes with %i points to evalueate', m_point_to_evaluate);
-    title(title_name);
-    %Just plot
-    [~,n] =size(size_matrix);
-    txt = "size matrix :" + string(size_matrix(:).^2);
-    txt= num2cell(txt);
-    h = zeros(n,1);
-    figure(3);
-    h(1) = plot(threads(begining:m),speedup(:,1), "-o");
-    title_name = sprintf("Graph of Computational Speedup vs Number of Threads for various Instance Sizes with %i points to evalueate", m_point_to_evaluate);
-    title(title_name);
-    ylabel('Speedup');
-    xlabel('Number of workers');
-    hold on;
-    for i = 2:n
-        figure(3);
-        h(i) = plot(threads(begining:m),speedup(:,i), '-o');
-    end
-    legend(txt{:});
-    hold off;
-
-     %Just plot
-    [~,n] =size(size_matrix);
-    txt = "size matrix :" + string(size_matrix(:).^2);
-    txt= num2cell(txt);
-    h = zeros(n,1);
-    figure(4);
-    h(1) = plot(threads(begining:m),efficiency(:,1), "-o");
-    title_name = sprintf("Graph of Computational efficiency vs Number of Threads for various Instance Sizes with %i points to evalueate", m_point_to_evaluate);
-    title(title_name);
-    ylabel('Efficiency');
-    xlabel('Number of workers');
-    hold on;
-    for i = 2:n
-        figure(3);
-        h(i) = plot(threads(begining:m),efficiency(:,i), '-o');
-    end
-    legend(txt{:});
-    hold off;
-
-
-    % number of mesurement realised
-    %[~,n] =size(size_matrix);
-    
-    % vector of plots for legend
-    %h = zeros(n,1);
-    %txt = "size matrix : " + string(size_matrix(1));
-    % generate plots
-    %time_mesurment = time_mesurment * 
-    
-    %disp(time_mesurment);
-
-    %h(1) = plot(gui,threads,log10(time_mesurment(1,:)),'DisplayName',txt);
-    
-    %title('Measured time');
-    %xlabel('Number of thread used');
-    %ylabel('log10 of the measured time in ms');
-    %hold(gui,'on');
-    %for i = 2:n
-     %   txt = "size matrix : " + string(size_matrix(i));
-      %  h(i) = plot(gui,threads,log10(time_mesurment(i,:)),'DisplayName',txt);
-    %end
-    
-    %hold(gui,"off");
-    %legend(gui);
 end
 
